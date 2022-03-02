0aaabc42860eec5641428adc4d52bf1566fb6c0f
Manu <manurajeev.720@gmailcom>
2022-03-02 17:29:26 -0600

    Added search template

--- search/views.py
+++ search/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+data = {'movies':['A Beautiful Mind', 'The Martian', 'Wonder', 'Interstellar', 'Passengers']}
+
+
+def search_view(request):
+    return render(request, 'search/search.html', context=data)
+
