1979558638563be9b9ea3fd21bc5aa40a3d11244
Manu <manurajeev.720@gmailcom>
2022-03-04 01:06:59 -0600

    Clean up and UI Improvements

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class searchForm(forms.Form):
-    searchbox = forms.CharField(max_length=100)
+    searchbox = forms.CharField(max_length=100, label='')
+    searchbox.widget = forms.TextInput(attrs={'class': 'search_box', 'title': 'Search',})
