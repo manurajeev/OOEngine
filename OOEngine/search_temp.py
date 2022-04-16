a7fb670acb25ed842277e2a9a719b24df67149b1
Prasad <prasadshinde1120@gmail.com>
2022-04-16 17:49:15 -0500

    Add new features

--- OOEngine/search_temp.py
+++ OOEngine/search_temp.py
@@ -1,39 +1,45 @@
 
 import itertools
 import csv
-from urllib import response
-import nltk
 import string
-nltk.download('stopwords')
-from nltk.corpus import stopwords
 from pathlib import Path
 
+STOP_WORDS = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "but", "if", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "and", "or", "not"}
+
 base_path = Path(__file__).parent
-csv_path = (base_path / "movies.csv").resolve()
+csv_path = (base_path / "movies_new_dataset.csv").resolve()
 movies_file = open(csv_path, 'r', encoding='utf-8')
 
-MOVIES_LIMIT = 10000
-MOVIES_DATA = []
-MOVIES_KWIC_DATA = {}
-en_stops = set(stopwords.words('english'))
+MOVIES_LIMIT = 45000
 
 
+# Call this from wherever DB is being setup
 def read_from_csv():
-    csvreader = csv.reader(movies_file)
-    next(csvreader)
-    next(csvreader)
+    csv_reader = csv.reader(movies_file)
+    next(csv_reader)
+    next(csv_reader)
 
-    for row in itertools.islice(csvreader, MOVIES_LIMIT):
-        row = [row[0], row[3]]
-        MOVIES_DATA.append(row)
-        keywords = remove_stop_words(row[0] + ' ' + row[1]).split()
+    movies_data_list = []
+    movies_kw_data = {}
+    for row in itertools.islice(csv_reader, MOVIES_LIMIT):
+        movies_data_list.append(row)
+        keywords = remove_stop_words(row[1] + ' ' + row[3]).split()
 
         for keyword in keywords:
-            keyword = keyword.lower()
+            if keyword not in movies_kw_data:
+                movies_kw_data[keyword] = []
+            movies_kw_data[keyword].append(row[0])
 
-            if keyword not in MOVIES_KWIC_DATA:
-                MOVIES_KWIC_DATA[keyword] = []
-            MOVIES_KWIC_DATA[keyword].append(len(MOVIES_DATA) - 1)        
+    return movies_data_list, movies_kw_data
 
 
 def remove_stop_words(query):
@@ -42,28 +48,18 @@ def remove_stop_words(query):
     res = []
 
     for word in query: 
-        if word not in en_stops:
+        if word in {"AND", "OR", "NOT"} or word.lower() not in STOP_WORDS:
             res.append(word)
     return ' '.join(res)
 
 
+# Use the same method for autofill
 def search(query):
-    query = query.lower()
     query = remove_stop_words(query).split()
-    resIndices = []
-
-    for word in query:
-        if word in MOVIES_KWIC_DATA:
-            resIndices += MOVIES_KWIC_DATA[word]
-    resIndices = set(resIndices)
-    response = []
-    
-    for index in resIndices:
-        response.append(MOVIES_DATA[index])
-    
-    return response
+    # Get result from DB
+    # database.getResult(query)
 
 
-read_from_csv()
-print(search("Twelve Outrageous guests"))
+# read_from_csv()
+# print(search("Twelve Outrageous guests"))
 # print(remove_stop_words("Just When His World Is Back To Normal... He's In For The Surprise Of His Life!"))
