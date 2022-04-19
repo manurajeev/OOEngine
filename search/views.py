50231c18a1b4c5cbb210e563612784fb21b8111e
Arush Sharma <arushsharma91@gmail.com>
2022-04-18 20:58:58 -0500

    autofill added

--- search/views.py
+++ search/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from OOEngine import search_temp
 
-# Create your views here.
 
 def search_view(request):
     search_input = request.session.get('searchbox')
     data = search_temp.search(search_input)
+
     print(data)
     return render(request, 'search/search.html', context={'search_input':search_input, 'data':data})
 
