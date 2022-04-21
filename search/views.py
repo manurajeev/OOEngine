75c3aa3392042106120edf1597fbc34c73036f72
developNY <sksldi0@gmail.com>
2022-04-21 02:11:27 -0400

    Pagenation

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from OOEngine import search_temp
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -7,5 +8,10 @@ def search_view(request):
     search_input = request.session.get('searchbox')
     data = search_temp.search(search_input)
     print(data)
-    return render(request, 'search/search.html', context={'search_input':search_input, 'data':data})
+
+    page = request.GET.get('page', '1') #GET 방식으로 정보를 받아오는 데이터
+    paginator = Paginator(data, '5') #Paginator(분할될 객체, 페이지 당 담길 객체수)
+    page_obj = paginator.get_page(page) #페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+
+    return render(request, 'search/search.html', context={'search_input':search_input, 'data':page_obj})
 
