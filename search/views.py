36dff680e560b73e7a0118811cba0be94b693f15
Arush Sharma <arushsharma91@gmail.com>
2022-04-22 15:55:29 -0500

    clean up

--- search/views.py
+++ search/views.py
@@ -5,7 +5,4 @@ from OOEngine import search_temp
 def search_view(request):
     search_input = request.session.get('searchbox')
     data = search_temp.search(search_input)
-
-    print(data)
     return render(request, 'search/search.html', context={'search_input':search_input, 'data':data})
-
